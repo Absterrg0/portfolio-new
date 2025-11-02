3e0d71885b0b0e0363f782f3288250d57c82ed32
Absterrg0 <parvj5212@gmail.com>
2025-11-02 15:04:50 +0530

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "../styles/globals.css"
 import { Toaster } from "react-hot-toast"
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -95,6 +95,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         <Toaster />
         {children}
       </body>
